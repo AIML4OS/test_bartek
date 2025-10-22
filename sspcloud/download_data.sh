93e929d76eccbcab5d60c7a91f63c4d0475ce25f
bartekgrancow <b.grancow@gmail.com>
2025-10-22 10:29:24 +0000

    test

--- sspcloud/download_data.sh
+++ sspcloud/download_data.sh
@@ -25,4 +25,14 @@ fi
 
 # === Download a second file ===
 # Repeat the code above for each file
+DATA_PATH="s3/bartoszgrancow/test_bartek/cars.txt"
+echo "Downloading $DATA_PATH to $DEST_FILE..."
+mc cp "$DATA_PATH" "$DEST_DIR"
 
+# === Check success ===
+if [ $? -eq 0 ]; then
+  echo "✅ File downloaded successfully to $DEST_FILE"
+else
+  echo "❌ Failed to download file from $DATA_PATH"
+  exit 1
+fi
